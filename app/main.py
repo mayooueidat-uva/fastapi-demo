79632588dfdb28221f828a0a2ba908937e8559ba
phisherman7 <zvd6vz@virginia.edu>
2024-10-28 23:54:29 -0400

    square

--- app/main.py
+++ app/main.py
@@ -22,7 +22,7 @@ def multiply(c: int, d: int):
 
 @app.get("/square/{e}")
 def square(e: int):
-    return {"square": e^2}
+    return {"square": e**2}
 
 @app.get("/makeaname/{f}/{g}")
 def name(f: str, g: str):
